5f70bf0e925da6d6bc63e5517c1136a89a53619f
libaoming <libaoming@dongsport.com>
2016-06-13 17:17:17 +0800

    自定义侧滑菜单

---
+++ app/src/main/java/com/limb/customview/activity/CustSlidMenuActivity.java
@@ -0,0 +1,15 @@
+package com.limb.customview.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.limb.customview.R;
+
+public class CustSlidMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cust_slid_menu);
+    }
+}
